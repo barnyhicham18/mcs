5fcb131fee393401943acd497f570a8144a7bd51
elyacoub9@ <elyacoub9@github.com>
2025-09-19 18:15:08 +0000

    add logging and update the payment page

--- cloudcreator.js
+++ cloudcreator.js
@@ -10,7 +10,7 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: 'file.log' })
+    new winston.transports.File({ filename: 'cloudspace.log' })
   ]
 });
 
